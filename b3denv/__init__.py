bd456466a760402530c3566a8b9aa9bf680fda6b
Rob Stenson <rob.stenson@gmail.com>
2022-11-09 16:25:12 -0800

    download action

--- b3denv/__init__.py
+++ b3denv/__init__.py
@@ -60,6 +60,52 @@ def get_vars(addon_name):
         }
 
 
+def fill_out_python(vars):
+    bpy = vars.get("bpy")
+    bpy_version = subprocess.check_output([bpy, "--version"]).split(" ")[-1].strip()
+
+    #import urllib.request
+    import shutil
+    import sys
+    import requests, tarfile, tempfile
+
+    package = os.path.dirname(os.path.abspath(__file__))
+    versions_folder = os.path.join(package, "versions")
+    version_folder = os.path.join(versions_folder, bpy_version)
+    if not os.path.exists(versions_folder):
+        os.mkdir(versions_folder)
+    if not os.path.exists(version_folder):
+        os.mkdir(version_folder)
+
+    def download(version):
+        tgz = "".join(["https://www.python.org/ftp/python/", version, "/Python-", version, ".tgz"])
+        print(tgz)
+        response = requests.get(tgz, stream=True)
+        
+        file = tarfile.open(fileobj=response.raw, mode="r|gz")
+
+        file.extractall(path=version_folder)
+        src = os.path.join(version_folder, ("Python-" + version + "/Include"))
+        dst_include = os.path.join(os.path.dirname(os.path.dirname(bpy)),  "include")
+        dst = os.path.join(dst_include, os.listdir(dst_include)[0])
+
+        for f in os.listdir(src):
+            src_fp = os.path.join(src, f)
+            dst_fp = os.path.join(dst, f)
+
+            print("---")
+            print(src_fp)
+            print(dst_fp)
+
+            if os.path.isdir(src_fp):
+                shutil.copytree(src_fp, dst_fp)
+            else:
+                shutil.copy2(src_fp, dst_fp)
+
+    download(bpy_version)
+    print("done")
+
+
 def install(vars):
     addon_source = vars.get("addon_source")
     addon = vars.get("addon")
@@ -125,6 +171,8 @@ def main():
         subprocess.call(["open", addon_path])
     elif action == "release":
         release(vars)
+    elif action == "download":
+        fill_out_python(vars)
 
 
 if __name__ == "__main__":
